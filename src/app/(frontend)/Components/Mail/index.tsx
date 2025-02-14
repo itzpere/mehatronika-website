237a68250b18f875d6b3f8fa558b894a11573cef
Petar Miletic <04.petar.miletic@gmail.com>
2025-02-14 12:54:14 +0100

    feat: add react-intersection-observer for improved animation triggers in MailSubscription component

--- src/app/(frontend)/Components/Mail/index.tsx
+++ src/app/(frontend)/Components/Mail/index.tsx
@@ -1,15 +1,12 @@
 'use client'
 
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import NewsletterForm from './components/form'
 import { subscribeToNewsletter } from './libs/post'
+import { useInView } from 'react-intersection-observer'
 
 const MailSubscription = () => {
-  const [isClient, setIsClient] = useState(false)
-
-  useEffect(() => {
-    setIsClient(true)
-  }, [])
+  const { ref, inView } = useInView({ triggerOnce: true, threshold: 0.1 })
 
   const handleSubmit = async (email: string, years: number[], subscribeToBoard: boolean) => {
     const result = await subscribeToNewsletter(email, years, subscribeToBoard)
@@ -24,13 +21,13 @@ const MailSubscription = () => {
   `
 
   const titleClasses = `
-    ${isClient ? 'animate-fade-up' : ''} 
+    ${inView ? 'animate-fade-up' : ''} 
     bg-gradient-to-r from-primary via-primary/80 to-secondary 
     bg-clip-text pb-2 text-3xl font-bold text-transparent sm:text-4xl md:text-5xl
   `
 
   const descriptionClasses = `
-    mx-auto mb-8 mt-4 max-w-xl ${isClient ? 'animate-fade-up' : ''} 
+    mx-auto mb-8 mt-4 max-w-xl ${inView ? 'animate-fade-up' : ''} 
     text-lg text-text/60 motion-safe:animate-fade-up
   `
 
@@ -40,21 +37,20 @@ const MailSubscription = () => {
         <div className="h-full w-full bg-gradient-to-br from-primary/20 to-secondary/20 blur-3xl" />
       </div>
 
-      <div className={containerClasses}>
+      <div ref={ref} className={containerClasses}>
         <div className="text-center">
           <h2 className={titleClasses}>Prijavite se na naš Newsletter</h2>
-
           <p
             className={descriptionClasses}
-            style={isClient ? ({ '--animation-delay': '200ms' } as React.CSSProperties) : {}}
+            style={inView ? ({ '--animation-delay': '200ms' } as React.CSSProperties) : {}}
           >
             Dobijajte obaveštenja o novim skriptama i rezultatima ispita
           </p>
         </div>
 
         <div
-          className={isClient ? 'animate-fade-up' : ''}
-          style={isClient ? ({ '--animation-delay': '400ms' } as React.CSSProperties) : {}}
+          className={inView ? 'animate-fade-up' : ''}
+          style={inView ? ({ '--animation-delay': '400ms' } as React.CSSProperties) : {}}
         >
           <NewsletterForm onSubmit={handleSubmit} />
         </div>
